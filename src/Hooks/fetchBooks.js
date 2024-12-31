0b0dcff79fcb86803c19f74ea810c3114f819d75
Malinhara <poornamalinhara53@gmail.com>
2024-12-31 09:28:24 +0530

    env update

--- src/Hooks/fetchBooks.js
+++ src/Hooks/fetchBooks.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+
+
 import { useEffect, useState } from 'react';
 
 const useBooks = (query = 'fiction', maxResults = 20, currentPage = 1) => {
@@ -12,7 +14,7 @@ const useBooks = (query = 'fiction', maxResults = 20, currentPage = 1) => {
       setLoading(true);
       try {
         const startIndex = (currentPage - 1) * maxResults; // Calculate startIndex for pagination
-        const response = await axios.get('http://localhost:3001/book/getall', {
+        const response = await axios.get(`${process.env.BACKENDURL}`, {
           params: { query, maxResults, startIndex },  // Pass parameters to the API
         });
 
